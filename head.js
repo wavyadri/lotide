196ec994a8dfb0c489b20c643fc432a0f23c59f0
Adriana <adriana.calvomatos@gmail.com>
2021-09-13 20:42:56 +0000

    Implement head to find first element in array

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = (arr) => {
+  return arr[0];
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hell');
+assertEqual(head([]), undefined);
+assertEqual(head([1]), 1);
