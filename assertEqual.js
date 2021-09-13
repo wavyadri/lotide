04f498a793d9d4874e0488cb03c7a362deb01730
Adriana <adriana.calvomatos@gmail.com>
2021-09-13 20:11:29 +0000

    Completed assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// MDN :
+// The console.assert() method writes an error message to the console if the assertion is false. If the assertion is true, nothing happens.
+
+// Our version will assert both pass and fail
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
