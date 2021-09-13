bfdbe620320d6325550f99946ea41e0060bd9407
Adriana <adriana.calvomatos@gmail.com>
2021-09-13 20:50:28 +0000

    Update test cases

--- head.js
+++ head.js
@@ -13,4 +13,3 @@ const head = (arr) => {
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hell');
 assertEqual(head([]), undefined);
-assertEqual(head([1]), 1);
