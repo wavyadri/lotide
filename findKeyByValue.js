1e3e126be80fc390da0ca09fc4ce69aeb0d5f0c5
Adriana <adriana.calvomatos@gmail.com>
2021-09-15 19:40:12 +0000

    completed findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (object, value) => {
+  // returns an array of all the keys
+  let keys = Object.keys(object);
+
+  for (let key of keys) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: 'The Expanse',
+  comedy: 'Brooklyn Nine-Nine',
+  drama: 'The Wire',
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Wire'), 'drama');
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
