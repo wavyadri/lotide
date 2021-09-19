3f71d233fdcd070959e3bd3c4f53e3c1756cd13f
Adriana <adriana.calvomatos@gmail.com>
2021-09-19 07:03:20 +0000

    refactor head to export to headTest for all test cases

--- head.js
+++ head.js
@@ -1,9 +1,5 @@
-const assertEqual = require('./assertEqual');
-
 const head = (arr) => {
   return arr[0];
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hell');
-assertEqual(head([]), undefined);
+module.exports = head;
