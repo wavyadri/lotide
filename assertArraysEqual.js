46b98b9d1375cc6aa62144612460d90920075167
Adriana <adriana.calvomatos@gmail.com>
2021-09-19 07:18:42 +0000

    refactor assertArraysEqual to export itself and import eqArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,4 @@
-const eqArrays = (arr1, arr2) => {
-  // check if arrays are equal in length
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  // check if array values are equal at the same index
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2)) {
@@ -23,3 +10,5 @@ const assertArraysEqual = (arr1, arr2) => {
 
 assertArraysEqual([1, 2, 3], [1, 2]); // false
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // true
+
+module.export = assertArraysEqual;
