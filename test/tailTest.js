a07b445eeaf09586d2c27c8b25491de46366e64b
Adriana <adriana.calvomatos@gmail.com>
2021-09-19 10:55:34 +0000

    Remove old assertions from tail

--- test/tailTest.js
+++ test/tailTest.js
@@ -39,22 +39,3 @@ describe('#tail', () => {
     assert.deepEqual(tail([]), []);
   });
 });
-
-// // Test Case 1: Check the returned array elements
-// const result = tail(['Hello', 'Lighthouse', 'Labs']);
-// assertEqual(result.length, 2); // ensure we get back two elements
-// assertEqual(result[0], 'Lighthouse'); // ensure first element is "Lighthouse"
-// assertEqual(result[1], 'Labs'); // ensure second element is "Labs"
-
-// // Test Case 2: Check the original array
-// const words = ['Yo Yo', 'Lighthouse', 'Labs'];
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// // Test Case 3: One ele
-// const one = tail(['One']);
-// assertEqual(one.length, 0);
-
-// // Test Case 4: Empty array
-// const empty = tail([]);
-// assertEqual(empty.length, 0);
