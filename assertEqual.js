68e0444740a5d9cfee13bb893e81a81086e517d5
Adriana <adriana.calvomatos@gmail.com>
2021-09-19 06:52:50 +0000

    refactor assertEqual to export and head to import assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -2,7 +2,7 @@
 // The console.assert() method writes an error message to the console if the assertion is false. If the assertion is true, nothing happens.
 
 // Our version will assert both pass and fail
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -13,3 +13,5 @@ const assertEqual = function(actual, expected) {
 // TEST CODE
 assertEqual('Lighthouse Labs', 'Bootcamp');
 assertEqual(1, 1);
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = (arr) => {
   return arr[0];
